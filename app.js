c95b713da0ef83afe0ca6430943a1937f5495685
senayWondimteka <senay.wendimteka@gmail.com>
2020-10-30 12:41:45 +0300

    Add button pop up works.

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+/*
 let button = document.querySelector('#addButton');
 
 button.addEventListener('click', creatForm);
 
+*/
 
-function creatForm() {
-    let addBookForm = document.createElement('form');
 
-    let text = prompt("Please Enter the book title: ");
-    myLibrary.push(new Book(text, "De", 2000, false));
+// Validating Empty Field
+function check_empty() {
+    if (document.getElementById('title').value == "" || document.getElementById('author').value == "" || document.getElementById('publishedYear').value == "") {
+    alert("Fill All Fields !");
+    } else {
+   document.getElementById('form').submit();
+    alert("Book Added Successfully...");
+    }
 }
+//Function To Display Popup
+function showAddForm() {
+    document.getElementById('addBook').style.display = "block";
+}
+    //Function to Hide Popup
+function hideAddForm(){
+    document.getElementById('addBook').style.display = "none";
+}
+
 
 
 let Book = function Book(title, author, publishedYear, read) {
@@ -17,9 +32,9 @@ let Book = function Book(title, author, publishedYear, read) {
     this.publishedYear = publishedYear, 
     this.read = function() { 
         if(!this.read) {
-             return "Sorry, I have not read" +  `${this.title}`;
+             return "Sorry, I have not read" + '"' + `${this.title}` + '"';
         } else {
-            return "Thank You for your offer! I have read " + `${this.title}`;
+            return "Thank You for your offer! I have read " + '"' + `${this.title}` +'."';
         }
     }
 }
@@ -36,7 +51,7 @@ function addBookToLibrary(Book) {
     }
 
     if(notInTheLibrary) {
-        return "Sorry, we aleardy have " + `${Book.title}` + " in our Library. No need to add";
+        return "Sorry, we aleardy have " + '"' + `${Book.title}` + '"' + " in our Library. No need to add";
     }else {
         myLibrary.push(Book);
         return "Successfully added to the library.";
@@ -52,14 +67,18 @@ function displayBooks()
    */
    for(let i = 0; i < myLibrary.length; i++) {
         let row = document.createElement('div');
+        row.classList.add('card');
         let title = document.createElement('h4');
+        title.classList.add('title');
         title.textContent = myLibrary[i].title;
         let author = document.createElement('p');
         author.textContent = myLibrary[i].author;
         let year = document.createElement('p');
         year.textContent = myLibrary[i].publishedYear;
-        let read = document.createElement('p');
+        let read = document.createElement('button');
+        //read.classList.add('read-status');
         read.textContent = myLibrary[i].read();
+        
         
 
         row.appendChild(title);
@@ -72,9 +91,11 @@ function displayBooks()
 
 }
 let bookOne = new Book("Atomic Habits", "James Clear", 2016, true);
+let bookTwo = new Book("The 7 Habits of Highly Effective People", "Steve Core", 2016, true);
 
 let myLibrary = [ ];
 
 myLibrary.push(bookOne);
+myLibrary.push(bookTwo);
 
 displayBooks();
